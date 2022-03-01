bbb7d154701dc4802cb696102e89fe2a1e17e50c
NanguRepo <nanguthenangu@gmail.com>
2022-03-01 18:44:47 +0100

    Trade calculator should be finished!

--- static/scripts/calculator.js
+++ static/scripts/calculator.js
@@ -12,6 +12,7 @@ function addItem(name1, side) {
     let amount = $(`#${side} #${name}`).attr("amount");
     $(`#${side} #${name}`).attr("amount", parseInt(amount)+1);
     $(`#${side} #${name}_amount`).html(parseInt(amount)+1);
+    updateResult();
     return;
   }
   panel.append(
@@ -27,9 +28,11 @@ function removeItem(name1, side) {
     $(`#${side} #${name}`).attr("amount", parseInt(amount)-1);
     if (parseInt(amount)-1 === 1){
       $(`#${side} #${name}_amount`).empty();
+      updateResult();
       return;
     }
     $(`#${side} #${name}_amount`).html(parseInt(amount)-1);
+    updateResult();
     return;
   }
   $(`#${side} #${name}`).remove();
@@ -42,7 +45,7 @@ function updateResult() {
       `
       <div class="columns is-mobile is-multiline is-centered">
         <div class="column is-half">
-          <div class="card">
+          <div id="tradeStats" class="card">
             <header class="card-header has-background-danger">
               <p class="card-header-title">
                 Trade Stats
@@ -55,7 +58,103 @@ function updateResult() {
         </div>
       </div>
       `
-    )
+    );
   }
+  Promise.all([getValues('left'), getValues('right')])
+  .then(data => {
+    let leftValue = 0;
+    let leftValueW = "NO!";
+    let leftDemandInfo = [0, 0];
+    let leftDemandW = "NO!";
+    let rightValue = 0;
+    let rightDemandInfo = [0, 0];
+    try {
+      data[0].forEach((item) => {
+        leftValue += parseInt(item['VALUE']);
+        leftDemandInfo[0] += parseInt(item['DEMANDNUMBER']);
+        leftDemandInfo[1]++;
+      });
+      data[1].forEach((item) => {
+        rightValue += parseInt(item['VALUE'])
+        rightDemandInfo[0] += parseInt(item['DEMANDNUMBER']);
+        rightDemandInfo[1]++;
+      });
+      var leftAverageDemand = leftDemandInfo[0]/leftDemandInfo[1];
+      var rightAverageDemand = rightDemandInfo[0]/rightDemandInfo[1];
+      if (leftValue < rightValue) {
+        leftValueW= "YES!";
+      };
+      if (leftAverageDemand < rightAverageDemand) {
+        leftDemandW = "YES!";
+      };
+      if (leftValue == rightValue) {
+        leftValueW= "EVEN!";
+      };
+      if (leftAverageDemand == rightAverageDemand) {
+        leftDemandW = "EVEN!";
+      };
+    } catch {
+      $("#statContent").empty();
+    };
+    $("#statContent").html(
+      `
+        <table class="table" id="stattable">
+          <tr>
+            <th></th>
+            <th>YOUR OFFER</th>
+            <th>THEIR OFFER</th>
+            <th>W FOR YOU?</th>
+          </tr>
+          <tr>
+            <th>Value</th>
+            <th>${leftValue}</th>
+            <th>${rightValue}</th>
+            <th>${leftValueW}</th>
+          </tr>
+          <tr>
+            <th>Average demand</th>
+            <th>${leftAverageDemand} stars</th>
+            <th>${rightAverageDemand} stars</th>
+            <th>${leftDemandW}</th>
+          </tr>
+        </table>
+      `
+    );
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+function getValues(side) {
+  return new Promise(function(resolve, reject) {
+    var ids = $('#' + side).children().map(function(){
+      return $(this).attr('id');
+      }).get();
+    
+    ids.forEach(function(id) {
+      let amount = $(`#${side} #${id}`).attr("amount");
+      if (amount !== "1") {
+        for (var i = 1; i < amount; i++) {
+          ids.push(id);
+        }
+      }
+    })
+    
+    let names = ids.join(",");
+  
+    fetch('https://api.nangurepo.com/v2/assassin?name=' + names)
+    .then(response => response.json())
+    .then(data => {
+        if (names.includes(",")) {
+          resolve(data);
+        } else {
+          resolve([data[0]]);
+        }
+    })
+    .catch(err => {
+      reject(err);
+    });
+  })
   
 }
