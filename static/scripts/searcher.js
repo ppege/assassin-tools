d9371c1ddc62c34817297553549f2d1211219e01
NanguRepo <nanguthenangu@gmail.com>
2022-03-01 15:23:31 +0100

    trade calculator

--- static/scripts/searcher.js
+++ static/scripts/searcher.js
@@ -22,8 +22,22 @@ function doneTyping() {
     .then(data => {
         data.forEach(function(knife) {
             $('#valuetable tr:last').after(
-                `<tr><th><img width=96 height=96 src="images/${knife['NAME'].replace(' ', '_')}.png"></th><th>${knife['NAME']}</th><th>${knife['DEMAND']}</th><th>${knife['VALUE']}</th><th>${knife['OBTAIN']}</th><th>${knife['ORIGIN']}</th></tr>`
+                `<tr><th><img width=96 height=96 src="images/${knife['NAME'].replace(/ /g,"_")}.png"></th><th>${knife['NAME']}</th><th>${knife['DEMAND']}</th><th>${knife['VALUE']}</th><th>${knife['OBTAIN']}</th><th>${knife['ORIGIN']}</th><th><button class="button" onclick="addItem('${knife['NAME']}', 'left')">LEFT</button><button class="button" onclick="addItem('${knife['NAME']}', 'right')">RIGHT</button></th></tr>`
             );
         })
     });
+}
+
+function addItem(name, side) {
+  let panel = $("#" + side)
+  if (panel.children().length === 6) {
+    return;
+  }
+  panel.append(
+    `<div data-tooltip="${name}" onclick="removeItem('${name.replace(/ /g,"_")}', '${side}')" id="${name.replace(/ /g,"_")}" class="tile is-child has-tooltip-bottom is-2"><img height=128 width=128 src="images/${name.replace(/ /g,"_")}.png"></div>`
+  )
+}
+
+function removeItem(name, side) {
+  $("#" + side + " #" + name + ":first").remove()
 }
