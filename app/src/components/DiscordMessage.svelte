262538f865bbf2de8b3183d8fc45f558b198742a
NanguRepo <nanguthenangu@gmail.com>
2022-09-22 18:10:48 +0200

    ad generator polish

--- app/src/components/DiscordMessage.svelte
+++ app/src/components/DiscordMessage.svelte
@@ -4,21 +4,38 @@
         message: string;
     }
     export let data: data;
-    let time = new Date();
-    $: hours = time.getHours();
-    $: minutes = time.getMinutes();
 </script>
 
-<div class="bg-[#36393f] p-4 rounded text-white max-w-[40ch]">
-    <div class="flex flex-row items-center gap-1">
-        <p class="font-semibold">
-            {data.username.charAt(0).toUpperCase() + data.username.slice(1)}
-        </p>
-        <p class="text-sm text-gray-400">{hours}:{minutes}</p>
+<div class="bg-[#36393f] p-3 rounded-lg text-white max-w-[40ch]">
+    <div class="flex flex-row gap-4">
+        <img
+            class="rounded-full w-10 h-10"
+            src="https://archive.org/download/discordprofilepictures/discordgrey.png"
+            alt=""
+        />
+        <div>
+            <div class="flex flex-row items-center gap-1">
+                <p class="font-semibold">
+                    {data.username.charAt(0).toUpperCase() +
+                        data.username.slice(1)}
+                </p>
+                <p class="text-xs text-gray-400">
+                    Today at {new Date()
+                        .toLocaleDateString("en-us", {
+                            hour: "numeric",
+                            minute: "2-digit",
+                        })
+                        .slice(-8)}
+                </p>
+            </div>
+            {#if data.message.includes("**Trading**")}
+                <p class="whitespace-pre-line">
+                    <b>Trading</b>
+                    {data.message.slice(11)}
+                </p>
+            {:else}
+                <p class="whitespace-pre-line">{data.message}</p>
+            {/if}
+        </div>
     </div>
-    {#if data.message.includes("**Trading**")}
-        <p class="whitespace-pre-line"><b>Trading</b> {data.message.slice(11)}</p>
-    {:else}
-        <p class="whitespace-pre-line">{data.message}</p>
-    {/if}
 </div>
